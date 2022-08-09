5151206503454ae74b67c67d3e20ad72e7ad3a1d
BaffIrodov <sergej.davidyuk@yandex.ru>
2022-08-09 20:02:07 +0300

    методы для гаусса

--- src/main/java/com/gen/GeneralModuleCalculating/services/DebugService.java
+++ src/main/java/com/gen/GeneralModuleCalculating/services/DebugService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -29,10 +30,47 @@ public class DebugService {
         try {
             PrintWriter out = new PrintWriter("D:/test/filename.txt");
             List<Float> result = queryFactory.from(playerOnMapResults).select(path).fetch();
+            getAverageOfList(result);
+            getSliceByPercent(95, 100, result);
+            getGroupForGauss(10, result);
             out.print(result.get(0));
         } catch (Exception e) {
             //nothing
         }
         return new ArrayList<>();
+    }
+
+    private List<Float> getSliceByPercent(int minPercent, int maxPercent, List<Float> source) {
+        Collections.sort(source);
+        List<Float> result = new ArrayList<>();
+        int minIndex = (source.size()/100) * minPercent;
+        int maxIndex = (source.size()/100) * maxPercent;
+        int index = 0;
+        for(Float item : source) {
+            if(minIndex <= index && index <= maxIndex) {
+                result.add(item);
+            }
+            index++;
+        }
+        return result;
+    }
+
+    //просто для красивого графика делаем подлог в данных
+    private List<Float> getGroupForGauss(int parts, List<Float> source) {
+        Collections.sort(source);
+        int indexOfPart = source.size() / parts;
+        for(int i = 0; i < parts; i++) {
+            int minIndex = indexOfPart * i;
+            int maxIndex = indexOfPart * (i+1);
+            float average = (source.get(minIndex) + source.get(maxIndex)) / 2;
+            for(int index = minIndex; index < maxIndex; index++) {
+                source.set(index, average);
+            }
+        }
+        return source;
+    }
+
+    private float getAverageOfList(List<Float> source) {
+        return (float) source.stream().mapToDouble(e -> e).average().orElse(0);
     }
 }
