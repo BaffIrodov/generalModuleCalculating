abec52b9e870bbc45d3c880235b588fc42dec254
BaffIrodov <sergej.davidyuk@yandex.ru>
2022-08-09 19:17:44 +0300

    fix

--- src/main/java/com/gen/GeneralModuleCalculating/controllers/CalculatingController.java
+++ src/main/java/com/gen/GeneralModuleCalculating/controllers/CalculatingController.java
@@ -3,6 +3,7 @@ package com.gen.GeneralModuleCalculating.controllers;
 import com.gen.GeneralModuleCalculating.dtos.MapsCalculatingQueueResponseDto;
 import com.gen.GeneralModuleCalculating.entities.Errors;
 import com.gen.GeneralModuleCalculating.services.CalculatingService;
+import com.gen.GeneralModuleCalculating.services.DebugService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,6 +22,9 @@ public class CalculatingController {
     @Autowired
     CalculatingService calculatingService;
 
+    @Autowired
+    DebugService debugService;
+
     @GetMapping("/create-queue")
     public MapsCalculatingQueueResponseDto createQueue() {
         return calculatingService.createQueue();
@@ -29,6 +33,12 @@ public class CalculatingController {
     @GetMapping("/current-queue-size")
     public MapsCalculatingQueueResponseDto getCurrentQueueSize(){
         return calculatingService.getCurrentQueueSize();
+    }
+
+    @GetMapping("/debug")
+    public void debug(){
+        debugService.init();
+//        calculatingService.debug();
     }
 
 }
--- src/main/java/com/gen/GeneralModuleCalculating/services/DebugService.java
+++ src/main/java/com/gen/GeneralModuleCalculating/services/DebugService.java
@@ -1,7 +1,38 @@
 package com.gen.GeneralModuleCalculating.services;
 
+import com.gen.GeneralModuleCalculating.entities.QPlayerOnMapResults;
+import com.querydsl.core.types.dsl.NumberPath;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DebugService {
+
+    @Autowired
+    JPAQueryFactory queryFactory;
+
+
+    private static final QPlayerOnMapResults playerOnMapResults =
+            new QPlayerOnMapResults("playerOnMapResults");
+
+    public List<Float> init() {
+        List<Float> result = getFullList(playerOnMapResults.adr);
+        return result;
+    }
+
+    public List<Float> getFullList(NumberPath<Float> path) {
+        try {
+            PrintWriter out = new PrintWriter("D:/test/filename.txt");
+            List<Float> result = queryFactory.from(playerOnMapResults).select(path).fetch();
+            out.print(result.get(0));
+        } catch (Exception e) {
+            //nothing
+        }
+        return new ArrayList<>();
+    }
 }
